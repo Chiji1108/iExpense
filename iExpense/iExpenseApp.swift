08d44c27c7c7d29902c76aa5f3f2b89e7ec3dd64
Shingo Chijiiwa <shingo.pad@gmail.com>
2024-06-04 21:21:44 +0900

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by 千々岩真吾 on 2024/06/04.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
